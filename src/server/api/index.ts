b5907a79635b322e19a1e967f938e93cb6b03368
idontreallywolf <nedim.kanat.lp@gmail.com>
2026-04-07 03:18:17 +0800

    install elysia-rate-limit mw

--- src/server/api/index.ts
+++ src/server/api/index.ts
@@ -2,6 +2,7 @@ import "~/logtape.config";
 
 import { elysiaLogger } from "@logtape/elysia";
 import Elysia from "elysia";
+import { rateLimit } from "elysia-rate-limit";
 
 import { linksRouter } from "./modules/links";
 
@@ -15,4 +16,5 @@ export const app = new Elysia({
          scope: "global",
       }),
    )
+   .use(rateLimit())
    .use(linksRouter);
